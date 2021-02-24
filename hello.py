a058d7643944d41cbf1bcf3b42a699285a32a032
heiqs <mrghanavati@gmail.com>
2021-02-24 07:43:08 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,5 +6,5 @@ def say_hello():
     logger = prefect.context.get("logger")
     logger.info("Hello, Cloud!")
 
-with Flow("hello-flow") as flow:
+with Flow("hello-flow-git") as flow:
     say_hello()
