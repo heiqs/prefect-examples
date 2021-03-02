f2f01db98d430de5ccc4ac56b5229f0860de32d1
heiqs <mrghanavati@gmail.com>
2021-03-02 15:04:25 +0100

    Update hello.py

--- flows/hello.py
+++ flows/hello.py
@@ -2,9 +2,11 @@ import prefect
 from prefect import task, Flow
 from prefect.storage import GitHub
 import os
+import time
 
 @task
 def say_hello():
+    time.sleep(600)
     logger = prefect.context.get("logger")
     logger.info(f"Hello, {os.getenv('USER')}!")
 
