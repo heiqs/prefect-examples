247d89bb3166b0b6cf452da17c0f717caa2d981c
heiqs <mrghanavati@gmail.com>
2021-03-07 21:54:21 +0100

    Update s3read.py

--- flows/s3read.py
+++ flows/s3read.py
@@ -18,24 +18,26 @@ client = boto3.client(
 
 @task
 def read_csv(bucket_name: str, object_key: str):
-    csv_obj = client.get_object(Bucket=bucket_name, Key=object_key)
+    s3 = boto3.resource(u's3')
+    bucket = s3.Bucket(bucket_name)
+    obj = bucket.Object(object_key)
+    csv_obj = obj.get()
+    # csv_obj = client.get_object(Bucket=bucket_name, Key=object_key)
     body = csv_obj['Body']
     csv_string = body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string))
-    logger = prefect.context.get("logger")
-    logger.info(df.head(5))
     return df.head(5)
 
 
-# @task
-# def write_csv(df, bucket_name: str, outfile: str):
-#     csv_buffer = StringIO()
-#     df.to_csv(csv_buffer)
+@task
+def write_csv(df, bucket_name: str, outfile: str):
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer)
 
-#     s3_resource = boto3.resource('s3')
-#     s3_resource.Object(bucket_name, outfile).put(Body=csv_buffer.getvalue())
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object(bucket_name, outfile).put(Body=csv_buffer.getvalue())
 
 
 with Flow('s3_read') as flow:
     df = read_csv('testmydask', 'data.csv')
-#     write_csv(df, 'testmydask', 'data-head.csv')
+    write_csv(df, 'testmydask', 'data-head.csv')
