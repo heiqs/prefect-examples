7c21018c39607cb9fddf045b610f23d72b534e15
argodis <info@argodis.de>
2021-10-06 09:55:29 +0200

    Update hello.py

--- flows/hello.py
+++ flows/hello.py
@@ -3,12 +3,11 @@ from prefect import task, Flow
 import os
 import time
 
-@task
-def say_hello(log_stdout=True):
+@task(log_stdout=True)
+def say_hello():
     logger = prefect.context.get("logger")
     logger.info(f"Hello, {os.getenv('username')}!")
-    print('This is an updated version!')
-    print(os.getenv('username'))
+    print(f"Hello, {os.getenv('username')}!")
 
 with Flow("hello-flow-git-with-env-var") as flow:
     say_hello()
